f5496056e06f8f532206d4c44dedb1e4c850888a
Hasan Haque <hasanavi@gmail.com>
2018-08-10 00:30:12 +0100

    add list into dom, more test cases

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,43 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			gifts: []
+		};
+	}
+
+	addGift = () => {
+		const { gifts } = this.state;
+
+		const ids = this.state.gifts.map(gift => gift.id);
+		const maxId = ids.length > 0 ? Math.max(...ids) : 0;
+
+		gifts.push({
+			id: maxId + 1
+		});
+
+		this.setState({ gifts });
+	};
+
 	render() {
 		return (
 			<div>
-				<h1>Gift Giver</h1>
+				<h1>React TDD</h1>
+				<h2>Gift Giver</h2>
+
+				<div className="gift-list">
+					{this.state.gifts.map(gift => {
+						return <div key={gift.id}>Boom</div>;
+					})}
+				</div>
+
+				<Button onClick={this.addGift} className="btn-add">
+					Add gift
+				</Button>
 			</div>
 		);
 	}
